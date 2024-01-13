e12951a3024bd4116e70cb0029b167cb7d0e4ec8
kirito0san <jodat.ahmedsyed@gmail.com>
2024-01-14 00:15:11 +0200

    update1/14

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LeaderShipTeam from "./components/LeaderShipTeam";
 function App() {
   return (
     <div className="h-screen bg-no-repeat bg-cover ">
-      <div className=" absolute top-0 left-0 w-full -z-10 h-screen"><img src={require(`./image/background.png`)} alt="" /></div>
+      <div className=" absolute top-0 left-0 w-full -z-10 h-screen"><img className="w-full h-full" src={require(`./image/background.png`)} alt="" /></div>
       <div className="bg-black ">
         <Header />
       </div>
